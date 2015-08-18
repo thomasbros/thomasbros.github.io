3486fcbf49682ccf06a74a9dd62602fc0a79f478
Xavier <xavier@xthomas.com>
2015-08-18 08:45:33 -0400

    Fixed the basic version 1 solution, without menu animation

--- gilead/ux/mobile/Home_files/axurerp_pagespecificscript.js
+++ gilead/ux/mobile/Home_files/axurerp_pagespecificscript.js
@@ -1,7 +1,39 @@
-﻿for(var i = 0; i < 2; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
+﻿for(var i = 0; i < 8; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
 
 $axure.eventManager.pageLoad(
 function (e) {
 
 });
-gv_vAlignTable['u1'] = 'center';
+gv_vAlignTable['u1'] = 'center';gv_vAlignTable['u4'] = 'center';u5.tabIndex = 0;
+
+u5.style.cursor = 'pointer';
+$axure.eventManager.click('u5', function(e) {
+
+if (true) {
+
+	SetPanelStateNext('u2',true,'none','',500,'none','',500);
+
+}
+});
+u6.tabIndex = 0;
+
+u6.style.cursor = 'pointer';
+$axure.eventManager.click('u6', function(e) {
+
+if (true) {
+
+	self.location.href='http://www.gilead.com/~/media/Files/pdfs/medicines/liver-disease/harvoni/harvoni_pi.pdf';
+
+}
+});
+u7.tabIndex = 0;
+
+u7.style.cursor = 'pointer';
+$axure.eventManager.click('u7', function(e) {
+
+if (true) {
+
+	self.location.href='http://hcp.harvoni.com';
+
+}
+});
