8b6fe4a4de5e42267261f92503da3c3251a158c8
ganesha <ganesha@module.home>
2016-02-06 22:23:38 -0500

    Ok, looks like I may have figured how to copy a directory to staging.

---
+++ tnp/v0.3/index_files/axurerp_pagespecificscript.js
@@ -0,0 +1,18 @@
+﻿for(var i = 0; i < 46; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
+
+$axure.eventManager.pageLoad(
+function (e) {
+
+});
+gv_vAlignTable['u20'] = 'top';gv_vAlignTable['u22'] = 'top';gv_vAlignTable['u23'] = 'top';gv_vAlignTable['u25'] = 'top';gv_vAlignTable['u26'] = 'top';gv_vAlignTable['u28'] = 'center';gv_vAlignTable['u30'] = 'center';gv_vAlignTable['u32'] = 'center';gv_vAlignTable['u34'] = 'center';u35.tabIndex = 0;
+
+u35.style.cursor = 'pointer';
+$axure.eventManager.click('u35', function(e) {
+
+if (true) {
+
+	self.location.href='#';
+
+}
+});
+gv_vAlignTable['u37'] = 'center';gv_vAlignTable['u39'] = 'center';gv_vAlignTable['u1'] = 'top';gv_vAlignTable['u3'] = 'top';gv_vAlignTable['u4'] = 'top';gv_vAlignTable['u6'] = 'center';gv_vAlignTable['u8'] = 'center';gv_vAlignTable['u40'] = 'top';gv_vAlignTable['u45'] = 'center';gv_vAlignTable['u43'] = 'center';gv_vAlignTable['u10'] = 'center';gv_vAlignTable['u11'] = 'top';gv_vAlignTable['u12'] = 'top';gv_vAlignTable['u13'] = 'top';gv_vAlignTable['u14'] = 'top';gv_vAlignTable['u17'] = 'top';gv_vAlignTable['u19'] = 'top';
