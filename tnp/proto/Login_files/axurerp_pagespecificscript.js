e069f9986c93699ee58cfc65a5e16757f82ec670
Xavier <xavier@xthomas.com>
2016-02-08 14:30:45 -0500

    Making a 'current' directory (called 'proto') so I don't have to update the bitly link

---
+++ tnp/proto/Login_files/axurerp_pagespecificscript.js
@@ -0,0 +1,66 @@
+﻿for(var i = 0; i < 35; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
+
+$axure.eventManager.pageLoad(
+function (e) {
+
+});
+gv_vAlignTable['u20'] = 'top';u21.tabIndex = 0;
+
+u21.style.cursor = 'pointer';
+$axure.eventManager.click('u21', function(e) {
+
+if (true) {
+
+SetGlobalVariableValue('loggedInVar', 'Yes');
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('Home.html');
+
+}
+});
+gv_vAlignTable['u23'] = 'center';gv_vAlignTable['u25'] = 'center';u26.tabIndex = 0;
+
+u26.style.cursor = 'pointer';
+$axure.eventManager.click('u26', function(e) {
+
+if (true) {
+
+SetGlobalVariableValue('loggedInVar', 'Yes');
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('Home.html');
+
+}
+});
+u27.tabIndex = 0;
+
+u27.style.cursor = 'pointer';
+$axure.eventManager.click('u27', function(e) {
+
+if (true) {
+
+SetGlobalVariableValue('loggedInVar', 'Yes');
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('Home.html');
+
+}
+});
+gv_vAlignTable['u28'] = 'top';document.getElementById('u31_img').tabIndex = 0;
+
+u31.style.cursor = 'pointer';
+$axure.eventManager.click('u31', function(e) {
+
+if (true) {
+
+}
+});
+gv_vAlignTable['u32'] = 'center';gv_vAlignTable['u33'] = 'top';gv_vAlignTable['u1'] = 'center';gv_vAlignTable['u3'] = 'center';gv_vAlignTable['u5'] = 'center';gv_vAlignTable['u7'] = 'center';gv_vAlignTable['u9'] = 'center';gv_vAlignTable['u10'] = 'top';gv_vAlignTable['u12'] = 'center';u13.tabIndex = 0;
+
+u13.style.cursor = 'pointer';
+$axure.eventManager.click('u13', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('Home.html');
+
+}
+});
+gv_vAlignTable['u15'] = 'center';gv_vAlignTable['u17'] = 'center';gv_vAlignTable['u19'] = 'center';
